36b5943eec9270a6a3e78161cdcecc7afa3631ce
clauron-seertech <cha@seertech.co>
2017-10-06 13:26:50 +0800

    - Add Log test

--- app/src/main/java/demo/seer/com/androiddeployautomation/MainActivity.java
+++ app/src/main/java/demo/seer/com/androiddeployautomation/MainActivity.java
@@ -2,14 +2,16 @@ package demo.seer.com.androiddeployautomation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
+    public String LAG = String.valueOf(MainActivity.this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // comment
+        Log.i(LAG, "test");
     }
 }
