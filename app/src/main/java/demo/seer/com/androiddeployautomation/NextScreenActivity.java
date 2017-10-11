83371a3a160449369208af6781a73c87f0444756
clauron-seertech <cha@seertech.co>
2017-10-11 14:38:32 +0800

    - Add new activity next screen - Add new layout for next screen activity

---
+++ app/src/main/java/demo/seer/com/androiddeployautomation/NextScreenActivity.java
@@ -0,0 +1,36 @@
+package demo.seer.com.androiddeployautomation;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by cha on 10/11/17.
+ */
+
+public class NextScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
